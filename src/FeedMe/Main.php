829aea26bac7f57f110af990e2dd88a62080e10b
SIVANNKH <39669152+SIVANNKH@users.noreply.github.com>
2018-06-08 23:27:18 -0700

    Add files via upload

---
+++ src/FeedMe/Main.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace FeedMe;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase{
+	
+	public function onEnable(){
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+		
+		switch($cmd->getName()){
+			
+			case "feedme":
+				if($sender instanceof Player){
+					
+					if($sender->hasPermission("feedme.command")){
+						$sender->setFood(20);
+						$sender->sendMessage("Your food has set!");
+					}
+					
+				}
+			break;
+			
+		}
+		return true;
+	}
+	
+}
