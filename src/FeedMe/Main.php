82187ca62435d2d7cc8aabca8e9296b98944e039
SIVANNKH <39669152+SIVANNKH@users.noreply.github.com>
2018-08-27 06:54:23 +0700

    Update Main.php

--- src/FeedMe/Main.php
+++ src/FeedMe/Main.php
@@ -12,7 +12,11 @@ use pocketmine\command\CommandSender;
 class Main extends PluginBase{
 	
 	public function onEnable(){
-	}
+        	$this->getLogger()->info("FeedMe by SIVANNKH has been enabled!");
+    	}
+    	public function onDisable(){
+        	$this->getLogger()->info("FeedMe by SIVANNKH has been disabled!");
+    	}
 	
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
 		
